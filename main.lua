e0cd2a126ed870337bd3f2f4626ee993517b41ad
“tjpaulino89” <tjpaulino89@gmail.com>
2022-11-08 20:22:58 -0500

    add three animals

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 function love.load()
     Object = require "classic"
     require "cow"
-    cow = Cow()
+
+    cow1 = Cow()
+    cow2 = Cow()
+    cow3 = Cow()
+    cows = {cow1, cow2, cow3}
+
 
     score = 0
 
@@ -9,13 +14,17 @@ function love.load()
 end
 
 function love.update(dt)
-    cow:update(dt)
+    cow1:update(dt)
+    cow2:update(dt)
+    cow3:update(dt)
 
 end
 
 function love.draw()
     love.graphics.setColor(255, 255, 255)
-    cow:draw()
+    cow1:draw()
+    cow2:draw()
+    cow3:draw()
 
     love.graphics.setFont(myFont)
     love.graphics.setColor(255, 0, 255)
@@ -24,11 +33,15 @@ end
 
 function love.mousepressed( x, y, button, istouch )
     if button == 1 then
-        local mouseToTarget = distanceBetween(x, y, cow.x, cow.y)
-        if mouseToTarget < cow.radius then
-        score = score + 1
-        cow.x = math.random(cow.radius, love.graphics.getWidth() - cow.radius)
-        cow.y = math.random(cow.radius, love.graphics.getHeight() - cow.radius)
+        for i = 1, 3 do
+            
+            local mouseToTarget = distanceBetween(x, y, cows[i].x, cows[i].y)
+            if mouseToTarget < cows[i].radius then
+            score = score + 1
+            cows[i].x = math.random(cows[i].radius, love.graphics.getWidth() - cows[i].radius)
+            cows[i].y = math.random(cows[i].radius, love.graphics.getHeight() - cows[i].radius)
+            end
+            
         end
     end
 end
@@ -36,3 +49,4 @@ end
 function distanceBetween(x1, y1, x2, y2)
     return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
+
